810d6d57c809f2df82398dace76ea7e1ace56470
Sparsh Bansal <sparshbansal787@gmail.com>
2025-06-11 08:58:53 +0530

    Perimeter of a Rectangle

---
+++ Week1/Rectangle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Rectangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the length of the rectangle: ");
+        double length = input.nextDouble();
+
+        System.out.print("Enter the width of the rectangle: ");
+        double width = input.nextDouble();
+        double perimeter = 2 * (length + width);
+        System.out.printf("The perimeter of the rectangle is: %.2f\n", perimeter);
+
+        input.close();
+    }
+}
