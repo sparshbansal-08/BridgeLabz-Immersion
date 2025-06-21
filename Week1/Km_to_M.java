bf70d03749197a374307325b19aa7c4155a861f5
Sparsh Bansal <127327393+sparshbansal-08@users.noreply.github.com>
2025-06-21 22:33:36 +0530

    Update Km_to_M.java

--- Week1/Km_to_M.java
+++ Week1/Km_to_M.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Km_to_M {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
 
 
         System.out.print("Enter distance in kilometers: ");
