3637a8140f80db0efe7841719a551310d6c07e96
Sparsh Bansal <127327393+sparshbansal-08@users.noreply.github.com>
2025-07-02 21:37:10 +0530

    Update Average.java

--- Week1/Average.java
+++ Week1/Average.java
@@ -4,6 +4,7 @@ public class Average {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        
         System.out.print("Enter the first number: ");
         double num1 = input.nextDouble();
 
